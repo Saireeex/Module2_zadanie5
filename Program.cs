184d492118814c8961cd7d8720946b250c77305b
SaIreeex . <stelkinrenat@gmail.com>
2024-09-30 09:15:27 +0300

    1.2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿using System;
+
+class TemperatureSensor
+{
+    private int temperature;
+
+    public event Action<int> TemperatureChanged;
+
+    public int Temperature
+    {
+        get => temperature;
+        set
+        {
+            if (temperature != value)
+            {
+                temperature = value;
+                OnTemperatureChanged(temperature);
+            }
+        }
+    }
+
+    protected virtual void OnTemperatureChanged(int temperature)
+    {
+        TemperatureChanged?.Invoke(temperature);
+    }
+}
+
+class Thermostat
+{
+    public Thermostat(TemperatureSensor sensor)
+    {
+        sensor.TemperatureChanged += OnTemperatureChanged;
+    }
+
+    private void OnTemperatureChanged(int temperature)
+    {
+        if (temperature < 20)
+            Console.WriteLine("Heating ON");
+        else
+            Console.WriteLine("Heating OFF");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TemperatureSensor sensor = new TemperatureSensor();
+        Thermostat thermostat = new Thermostat(sensor);
+
+        sensor.Temperature = 18; // Heating ON
+        sensor.Temperature = 10; // Heating OFF
+    }
+}
